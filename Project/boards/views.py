1aa43b61370e2c2b0fe1c9af0c543f05b599a6ca
cebastian <suangel.hilamo>
2022-11-28 10:32:12 -0500

    pj base

---
+++ Project/boards/views.py
@@ -0,0 +1,9 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+from .models import Board
+
+# Create your views here.
+def home(request):
+    boards = Board.objects.all
+    ()
+    return render(request, 'home.html', {'boards':boards})
